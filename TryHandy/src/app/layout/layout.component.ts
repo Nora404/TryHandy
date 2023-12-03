49d8aad4f29dee12ca8356c49ae83bc430f28577
ina-verena <144120729+ina-verena@users.noreply.github.com>
2023-12-03 14:22:01 +0100

    message

--- TryHandy/src/app/layout/layout.component.ts
+++ TryHandy/src/app/layout/layout.component.ts
@@ -9,6 +9,8 @@ import { AfterViewChecked, Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements AfterViewChecked {
 
+  test: string = "START";
+
   setTheme(theme: string) {
     document.body.classList.remove('light-theme', 'dark-theme');
     document.body.classList.add(theme);
@@ -25,15 +27,19 @@ export class LayoutComponent implements AfterViewChecked {
   }
 
   handleNord() {
+    this.test = 'NORD';
     console.log('NORD')
   }
   handleWest() {
+    this.test = 'WEST';
     console.log('WEST')
   }
   handleSud() {
+    this.test = 'SUD';
     console.log('SUD')
   }
   handleOst() {
+    this.test = 'OST';
     console.log('OST')
   }
 
