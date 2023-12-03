96ee9c6ff4f64eacfcf44f5bb80d0698115d11d8
ina-verena <144120729+ina-verena@users.noreply.github.com>
2023-12-03 12:55:11 +0100

    drei theme

---
+++ TryHandy/src/app/layout/layout.component.ts
@@ -0,0 +1,27 @@
+import { AfterViewChecked, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss'
+})
+export class LayoutComponent implements AfterViewChecked {
+
+  setTheme(theme: string) {
+    document.body.classList.remove('light-theme', 'dark-theme', 'custom-theme');
+    document.body.classList.add(theme);
+
+    localStorage.setItem('theme', theme);
+  }
+
+  ngAfterViewChecked(): void {
+    const theme = localStorage.getItem('theme') || 'light-theme';
+
+    if (document) {
+      document.body.classList.add(theme);
+    }
+  }
+
+}
